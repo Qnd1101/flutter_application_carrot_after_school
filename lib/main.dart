fece0c6e9b6363598a0381eb656bf22ab598d831
Minjun <mj061101k@naver.com>
2023-04-20 18:34:06 +0900

    navigator추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_carrot/detail_page.dart';
+import 'carrot_item.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,91 +13,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     items.add(CarrotItem(title: '응원봉 팔아요', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요2', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요3', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요4', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요5', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요6', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요7', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요8', addr: '성남시 중원구', price: 10000));
-    items.add(CarrotItem(title: '응원봉 팔아요9', addr: '성남시 중원구', price: 10000));
+    items.add(CarrotItem(title: '응원봉 팔아요2', addr: '성남시 중원구', price: 20000));
+    items.add(CarrotItem(title: '응원봉 팔아요3', addr: '성남시 중원구', price: 30000));
+    items.add(CarrotItem(title: '응원봉 팔아요4', addr: '성남시 중원구', price: 40000));
+    items.add(CarrotItem(title: '응원봉 팔아요5', addr: '성남시 중원구', price: 50000));
+    items.add(CarrotItem(title: '응원봉 팔아요6', addr: '성남시 중원구', price: 60000));
+    items.add(CarrotItem(title: '응원봉 팔아요7', addr: '성남시 중원구', price: 70000));
+    items.add(CarrotItem(title: '응원봉 팔아요8', addr: '성남시 중원구', price: 80000));
+    items.add(CarrotItem(title: '응원봉 팔아요9', addr: '성남시 중원구', price: 90000));
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Malob market'),
-          backgroundColor: Colors.orange,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [for (var item in items) item],
-          ),
-        ),
-      ),
+      home: HomePage(items: items),
     );
   }
 }
 
-class CarrotItem extends StatelessWidget {
-  String title, addr;
-  int price;
-  CarrotItem({
-    required this.title,
-    required this.addr,
-    required this.price,
+class HomePage extends StatelessWidget {
+  const HomePage({
     super.key,
+    required this.items,
   });
+
+  final List<CarrotItem> items;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: const DecorationImage(
-                  image: AssetImage('images/NewJeans.jpg'))),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Malob market'),
+        backgroundColor: Colors.orange,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            for (var item in items)
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailPage(price: item.price),
+                        ));
+                  },
+                  child: item) // 내 행동을 인식 한다.
+          ],
         ),
-        const SizedBox(
-          width: 20,
-        ),
-        Flexible(
-          flex: 1,
-          child: SizedBox(
-            height: 150,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title,
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold)),
-                // 제목 따로 줄
-                const Divider(),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  addr,
-                  style: const TextStyle(
-                      fontSize: 17,
-                      color: Colors.grey,
-                      decoration: TextDecoration.underline),
-                ),
-                Text('$price'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [Icon(Icons.heart_broken), Text('12')],
-                )
-              ],
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
